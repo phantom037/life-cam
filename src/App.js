7c8a74610a26090926fa2c230a881d8f877a8254
Leo Tran <58055271+phantom037@users.noreply.github.com>
2023-03-14 18:06:36 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import ReactImageGallery from "./ReactImageGallery";
 
 function App() {
   return (
+    <h1 style ={{textAlign: 'center'}}>Life Cam</h1>
+    <h2 style ={{textAlign: 'center'}}>Where I capture my memories</h2>
     <ReactImageGallery>
       
     </ReactImageGallery>
